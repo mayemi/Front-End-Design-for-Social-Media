613ff361a88a89ff2bbd889cd88ae76ef396e947
mayemi <kayrakayiran2009@gmail.com>
2024-08-23 16:14:38 +0300

    Main Files
    HTML, CSS, JavaScript and JPG files has been added.

---
+++ Sosyal Medya Ana Sayfa/script.js
@@ -0,0 +1,53 @@
+const users = {
+    "Kayra": {
+        name: "Kayra Çelik",
+        followers: "152 Takipçi",
+        following: "278 Takip",
+        description: "Merhabalar ben Kayra bu siteyi denemek için kurdum. Düzenlemelere devam edeceğim."
+    },
+    "Deniz": {
+        name: "Deniz Yılmaz",
+        followers: "200 Takipçi",
+        following: "300 Takip",
+        description: "Merhaba ben Deniz bu platformu kurulduğundan beri kullanıyorum."
+    },
+    "Ali": {
+        name: "Ali Veli",
+        followers: "180 Takipçi",
+        following: "150 Takip",
+        description: "Ben Ali."
+    },
+    "Doruk": {
+        name: "Doruk Demir",
+        followers: "120 Takipçi",
+        following: "220 Takip",
+        description: "Doruğa ulaştın."
+    },
+    "Derin": {
+        name: "Derin Su",
+        followers: "250 Takipçi",
+        following: "280 Takip",
+        description: "Derin bir profile giriş yaptın."
+    }
+};
+
+const profileCard = document.querySelector('.kart');
+const profileImage = profileCard.querySelector('img');
+const profileIcons = profileCard.querySelectorAll('i');
+
+document.querySelectorAll('.paylasim').forEach(post =>{
+    post.addEventListener('mouseover',()=>{
+        const user = post.querySelector('h2').textContent;
+        const userInfo = users[user];
+
+        profileCard.querySelector('.kartismin').textContent = userInfo.name;
+        profileCard.querySelector('.takiptakipci').textContent = `${userInfo.followers} ${userInfo.following}`
+        profileCard.querySelector('.kart > p:nth-of-type(2)').textContent = userInfo.description;
+
+        profileCard.style.display = 'flex';
+        profileImage.style.display = 'block';
+        profileIcons.forEach(icon => icon.style.display = 'inline-block');
+
+        profileCard.classList.add('fade-in')
+    });
+});
